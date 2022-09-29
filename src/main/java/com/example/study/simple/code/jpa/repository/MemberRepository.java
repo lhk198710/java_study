7ae1f43b15b5b50331237940fcfb51f240c4b93a
lhk871010 <lhk8710@lfcorp.com>
2022-09-29 14:11:16 +0900

    jpa study 코드 별도 분리를 위한 기존 파일 제거

--- src/main/java/com/example/study/simple/code/jpa/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.study.simple.code.jpa.repository;
-
-import com.example.study.simple.code.jpa.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-}
